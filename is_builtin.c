ea93daf4a31abaf93559d010fa46d0b6661823d2
Aalaa Satti <ala.k.satti@gmail.com>
2019-08-21 01:46:45 +0000

    ammended builtin checker

--- is_builtin.c
+++ is_builtin.c
@@ -3,33 +3,35 @@
 
 int is_builtin(char **cmd)
 {
-	int i;
+	int i = 0;
 
 	b_t built_in_list[] = {
 
 		{"env", print_env},
 		{"exit", exit_shell},
+/**
 		{"help", help},
 		{"history", history},
 		{"cd", change_dir},
 		{"alias", alias},
 		{"setenv", set_env},
 		{"unsetenv", unset_env},
+**/
+		{"", NULL},
 		{NULL, NULL},
 	};
 
-while (built_in_list)
-{
-	for (i = 0; built_in_list[i].cmd; i++)
-	{
-		if(!_strcmp(built_in_list[i].cmd, cmd))
-		{
-			built_in_list[i].func(cmd);
-			return (0);
-		}
-	}
-}
 
+while (built_in_list[i].cmd)
+{
+	if(!_strcmp(built_in_list[i].cmd, cmd[0]))
+	{
+		built_in_list[i].func(cmd);
+		return (0);
+	}
+
+	++i;
+}
 return (-1);
 
 }
