f5cf947497e47fefb4f690d4108e73c3f5f43249
Marco Chan <inspiredtolive0@gmail.com>
2019-08-27 19:58:00 +0000

    Modifies the correct string

--- search_path.c
+++ search_path.c
@@ -10,22 +10,16 @@ char *search_path(char *command, env_t *env)
 	dir = strtok(value, ":\n");
 	while (dir != NULL)
 	{
-		printf("%s\n", dir);
 		command_path = malloc(_strlen(dir) + _strlen(command) + 2);
 		if (command_path == NULL)
 			exit(1);
 		for (i = 0; dir[i]; i++)
 			command_path[i] = dir[i];
-		dir[i] = '/';
-		printf("full path: %s\n", command_path);
+		command_path[i] = '/';
 		i++;
 		for (j = 0; command[j]; j++)
-		{ 
-			printf("copy: %c into %d\n", command[j], i + j);
 			command_path[i + j] = command[j];
-		}
 		command_path[i + j] = '\0';
-		printf("full path: %s\n", command_path);
 		if (stat(command_path, &st) == 0)
 			return (command_path);
 		free(command_path);
